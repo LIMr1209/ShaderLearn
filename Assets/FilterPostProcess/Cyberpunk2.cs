d057aed810bf6f33160cdde6802aba230a64c6e6
LIMr1209 <aaa1058169464@126.com>
2022-10-28 16:01:48 +0800

    filter

---
+++ Assets/FilterPostProcess/Cyberpunk2.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+
+// 赛博朋克滤镜2
+public class Cyberpunk2 : PostEffectsBase
+{
+    public Shader CyberpunkShader;
+    private Material CyberpunkMaterial;
+
+    public Material material
+    {
+        get
+        {
+            CyberpunkMaterial = CheckShaderAndCreateMaterial(CyberpunkShader, CyberpunkMaterial);
+            return CyberpunkMaterial;
+        }
+    }
+
+    [Range(-1.0f, 0.5f)] public float red = 0;
+    [Range(-0.5f, 0.5f)] public float orange = 0;
+    [Range(-0.5f, 1.0f)] public float yellow = 0;
+    [Range(-1.0f, 1.0f)] public float green = 0;
+    [Range(-1.0f, 1.0f)] public float cyan = 0;
+    [Range(-1.0f, 0.5f)] public float blue = 0;
+    [Range(-0.5f, 0.5f)] public float purple = 0;
+    [Range(-0.5f, 1.0f)] public float magenta = 0;
+
+    [Range(1.0f, 10.0f)] public float pow = 0;
+
+    [Range(0.0f, 2.0f)] public float value = 0;
+
+
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetFloat("_Red", red);
+            material.SetFloat("_Orange", orange);
+            material.SetFloat("_Yellow", yellow);
+            material.SetFloat("_Green", green);
+            material.SetFloat("_Cyan", cyan);
+            material.SetFloat("_Blue", blue);
+            material.SetFloat("_Purple", purple);
+            material.SetFloat("_Magenta", magenta);
+            
+            
+            material.SetFloat("_Power", pow);
+            material.SetFloat("_Value", value);
+
+            //  src 纹理会传递给shader 中名为 _MainTex 的纹理属性 参数 pass 默认 -1 表示一次调用 pass , 否则只会调用指定索引的pass
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            // 没有材质 会直接把源图像 显示到屏幕上
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/FilterPostProcess/OliPaint.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+
+// 赛博朋克滤镜1
+public class OliPaint : PostEffectsBase
+{
+    public Shader OliPaintShader;
+    private Material OliPaintMaterial;
+
+    public Material material
+    {
+        get
+        {
+            OliPaintMaterial = CheckShaderAndCreateMaterial(OliPaintShader, OliPaintMaterial);
+            return OliPaintMaterial;
+        }
+    }
+
+    [Range(0, 10)] public int Radius = 3;
+
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            material.SetInt("_Radius", Radius);
+            material.SetVector("_PSize", new Vector2(1f / (float) src.width, 1f / (float) src.height));
+
+            //  src 纹理会传递给shader 中名为 _MainTex 的纹理属性 参数 pass 默认 -1 表示一次调用 pass , 否则只会调用指定索引的pass
+            Graphics.Blit(src, dest, material);
+        }
+        else
+        {
+            // 没有材质 会直接把源图像 显示到屏幕上
+            Graphics.Blit(src, dest);
+        }
+    }
+}
